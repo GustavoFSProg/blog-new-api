8f793f22d9a2500ce7fad00ac43e0d92c010395b
Gustavo <gustavo.prog40@gmail.com>
2022-09-04 12:46:30 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,16 @@ const app = express()
 const { PORT } = process.env
 
 app.use(express.json())
-app.use(cors())
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 app.use(route)
 
 app.get('/', function (req: Request, res: Response) {

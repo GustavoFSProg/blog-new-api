c116deea1a5bddb4fc61c45d6d41f477a2c9e294
Gustavo <gustavo.prog40@gmail.com>
2022-09-04 13:39:53 -0300

    afdasdfdsf

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,15 @@ const { PORT } = process.env
 
 app.use(express.json())
 
-    app.use(cors());
+
+
+app.use(cors())
 
 app.use(route)
 
 app.get('/', function (req: Request, res: Response) {
   return res.status(200).send({ msg: ` 🍏 Api Running` })
 })
-
 
 app.listen(PORT, () => {
   console.log(` 🍏 Api Running ${PORT}`)

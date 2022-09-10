741e9aaa536749b31d245a529ff78d6f72386719
Gustavo <gustavo.prog40@gmail.com>
2022-09-10 14:23:43 -0300

    configuring cors

--- src/Routes/userRoutes.ts
+++ src/Routes/userRoutes.ts
@@ -6,6 +6,7 @@ import { isAuthorized } from '../authorize'
 //   origin: 'https://blog-new.netlify.app/',
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
+import cors from 'cors'
 
 import userController from '../userController'
 
@@ -14,6 +15,6 @@ const UsersRoute = Router()
 UsersRoute.get('/all-user', isAuthorized, userController.getAll),
   // UsersRoute.post('/verify-token', userController.Token)
   UsersRoute.post('/register-user', userController.createUser)
-UsersRoute.post('/login', userController.Login)
+UsersRoute.post('/login', cors(), userController.Login)
 
 export default UsersRoute

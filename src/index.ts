3edc7961868f815877eb122a830a2913aba57536
Gustavo <gustavo.prog40@gmail.com>
2022-09-10 13:59:10 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,24 @@ dotenv.config()
 const app = express()
 
 const { PORT } = process.env
-  
-
 
 app.use(express.json())
 app.use(cors())
-// app.use(cors(
-//   "Access-Control-Allow-Headers": "Content-Type",
-//   "Access-Control-Allow-Origin": "https://www.example.com",
-//   "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
 
-// ))
+
+var corsOptions = {
+  origin: 'https://blog-new.netlify.app/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+// app.get('/', cors(corsOptions), function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for only example.com.'})
+// })
+
 
 app.use(route)
 
-app.get('/', function (req: Request, res: Response) {
+app.get('/', cors(corsOptions), function (req: Request, res: Response) {
   return res.status(200).send({ msg: ` 🍏 Api Running` })
 })
 

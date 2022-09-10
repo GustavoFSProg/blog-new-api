9c921c92b292fa38c7ad706000c5d566919fc2b8
Gustavo <gustavo.prog40@gmail.com>
2022-09-10 14:53:26 -0300

    fazendo

--- src/Routes/postRoutes.ts
+++ src/Routes/postRoutes.ts
@@ -11,6 +11,12 @@ import { isAuthorized } from '../authorize'
 const PostsRoute = Router()
 
 
+const Postproducts = {
+  origin: true,
+  methods: ["POST"],
+  credentials: true,
+  maxAge: 3600
+};
 // var corsOptions = {
 //   // allowedHeaders: ['Content-Type', 'Authorization'], 
 //   // allowedHeaders:['https://blog-new.netlify.app/'], 
@@ -21,9 +27,9 @@ const PostsRoute = Router()
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 
-PostsRoute.get('/all',  postController.getAll),
+PostsRoute.get('/all',   postController.getAll),
 PostsRoute.get('/search', postController.getSearch)
-PostsRoute.post('/register', isAuthorized, upload.single('image'),    postController.registerPost)
+PostsRoute.post('/register', isAuthorized,  cors(Postproducts), upload.single('image'),    postController.registerPost)
 
  
 

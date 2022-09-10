02f3e6ca9cfe1ddbbcc7bfc1de4ec24ad81d2053
Gustavo <gustavo.prog40@gmail.com>
2022-09-10 15:09:03 -0300

    login cors configs dres

--- src/Routes/postRoutes.ts
+++ src/Routes/postRoutes.ts
@@ -7,30 +7,40 @@ const upload = multer(uploadConfig)
 
 import postController from '../postController'
 import { isAuthorized } from '../authorize'
-        
+
 const PostsRoute = Router()
 
 const Postproducts = {
   origin: true,
-  methods: ["POST"],
+  methods: ['POST'],
   credentials: true,
-  maxAge: 3600
-};
+  maxAge: 3600,
+}
+
+var corsOptions = {
+  origin: 'https://blog-new.netlify.app/',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}
 
 // var corsOptions = {
-//   // allowedHeaders: ['Content-Type', 'Authorization'], 
-//   // allowedHeaders:['https://blog-new.netlify.app/'], 
-  
- 
+//   // allowedHeaders: ['Content-Type', 'Authorization'],
+//   // allowedHeaders:['https://blog-new.netlify.app/'],
+
 //   // origin: '  ',
- 
+
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 
-PostsRoute.get('/all',   postController.getAll),
-PostsRoute.get('/search', postController.getSearch)
-PostsRoute.post('/register',  cors(Postproducts), isAuthorized,  upload.single('image'),    postController.registerPost)
-
- 
+PostsRoute.get('/all', cors(corsOptions), postController.getAll),
+  PostsRoute.get('/search', postController.getSearch)
+PostsRoute.post(
+  '/register',
+  cors(Postproducts),
+  isAuthorized,
+  upload.single('image'),
+  postController.registerPost
+)
 
 export default PostsRoute

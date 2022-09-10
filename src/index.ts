25b91db21bc731fe65b7c634f5fee85aa3bfc11d
Gustavo <gustavo.prog40@gmail.com>
2022-09-10 09:01:48 -0300

    new corfs configuration

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,14 @@ const app = express()
 
 const { PORT } = process.env
 
+app.all('*', function(req, res, next) {
+     var origin = req.get('origin'); 
+     res.header('Access-Control-Allow-Origin', origin);
+     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     res.header('Access-Control-Allow-Headers', 'Content-Type');
+     next();
+});
+
 app.use(express.json())
 app.use(cors())
 // app.use(cors(

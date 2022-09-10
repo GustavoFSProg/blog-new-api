ddc3d39e0e42c7d7c5469bd5c77abbd74d3ea58d
Gustavo <gustavo.prog40@gmail.com>
2022-09-10 09:35:27 -0300

    config with cors

--- src/index.ts
+++ src/index.ts
@@ -20,22 +20,6 @@ app.use(cors())
 
 // ))
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-  })
-
 app.use(route)
 
 app.get('/', function (req: Request, res: Response) {

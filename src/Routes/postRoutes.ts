8d04aed494b12783dc9c6aa1b6f50f9032254f31
Gustavo <gustavo.prog40@gmail.com>
2022-11-04 19:35:56 -0300

    token

--- src/Routes/postRoutes.ts
+++ src/Routes/postRoutes.ts
@@ -6,40 +6,13 @@ import multer from 'multer'
 const upload = multer(uploadConfig)
 
 import postController from '../controllers/postController'
-import { isAuthorized } from '../utils/authorize'
 
 const PostsRoute = Router()
 
-const Postproducts = {
-  origin: true,
-  methods: ['POST'],
-  credentials: true,
-  maxAge: 3600,
-}
-
-var corsOptions = {
-  origin: ['https://blog-new.netlify.app/'],
-  // origin: ['http://lodescalhost:5173/'],
-  // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  // preflightContinue: false,
-  optionsSuccessStatus: 204,
-}
-
-// var corsOptions = {
-//   // allowedHeaders: ['Content-Type', 'Authorization'],
-//   // allowedHeaders:['https://blog-new.netlify.app/'],
-
-//   // origin: '  ',
-
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-// cors(corsOptions), 
-
-// PostsRoute.get('/all', cors(corsOptions), postController.getAll),
 PostsRoute.get('/all', postController.getAll),
 PostsRoute.get('/total', postController.getAll),
   PostsRoute.get('/search', postController.getSearch),
-PostsRoute.post('/register', isAuthorized, upload.single('image'), postController.registerPost),
+PostsRoute.post('/register', upload.single('image'), postController.registerPost),
 
 PostsRoute.put('/likes/:id', postController.updateLikes)
 PostsRoute.post('/views/:id', postController.updateViews)

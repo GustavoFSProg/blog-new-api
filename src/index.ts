b7d32b98d0ad87a4e8b6baffc011bc1c0b922741
Gustavo <gustavo.prog40@gmail.com>
2022-11-05 08:42:36 -0300

    cors 2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express'
 import cors from 'cors'
+import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import route from './Routes/routes'
 

edef11cb4dfebd8ba12e0ecbfdf0481ebb2c5129
Gustavo <gustavo.prog40@gmail.com>
2022-11-05 08:35:09 -0300

    token

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,17 @@ const app = express()
 
 const { PORT } = process.env
 
+// app.use(
+//   (_req: Request, res: Response, next: () => void) => {
+//     //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+//     res.header("Access-Control-Allow-Origin", "*")
+//     //Quais são os métodos que a conexão pode realizar na API
+//     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
+//     app.use(cors())
+//     next()
+//   })
 
-app.use(
-  (_req: Request, res: Response, next: () => void) => {
-    //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*")
-    //Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
-    res.header("Access-Control-Allow-Headers", "*") 
-    app.use(cors())
-    next()
-  })
-    
+app.use(cors())    
 app.use(express.json())
 app.use(route)
 
